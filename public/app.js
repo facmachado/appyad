0b13e4bdea5515efee39f969d3f19d54f8b6dc13
Flávio Augusto <facmachado@gmail.com>
2020-07-30 21:35:54 -0300

    Win98 main menu

--- public/app.js
+++ public/app.js
@@ -1,57 +1,38 @@
 /* jshint esversion: 8 */
 
-/* Desabilitar botão direito e arrastar */
 document.oncontextmenu = _ => false;
 document.ondragstart = _ => false;
 
-/* Desabilitar botão voltar do navegador */
 history.pushState(null, null, document.location.href);
 window.onpopstate = _ => history.go(+1);
 
 $(_ => {
-  $(window).on('blur', _ => {
-    $('.menu:not(.top)').hide();
-    $('.nav').prop('disabled', true);
-    $(document.body).addClass('disabled');
+  $(window).on({
+    blur:  _ => $('body').prop('disabled', true),
+    click: _ => {},
+    focus: _ => $('body').prop('disabled', false)
   });
 
-  $(window).on('click', _ => {
-    if ($('.menu').is(':visible') && !$('.menu button').is(':focus')) {
-      $('.menu:not(.top)').hide();
-    }
-  });
 
-  $(window).on('focus', _ => {
-    $(document.body).removeClass('disabled');
-    $('.nav').prop('disabled', false);
-  });
+  // $('button:not([cmd])').on('keyup', e => {
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  // });
 
-  $('button:not([cmd])').on('keyup', e => {
-    e.preventDefault();
-    e.stopPropagation();
-    $(e.target).click();
-  });
+  // $('button:not([cmd])').on('mouseover', e => {
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  //   // $(e.target).focus().click();
+  // });
 
-  $('button:not([cmd])').on('mouseover', e => {
-    e.preventDefault();
-    e.stopPropagation();
-    $(e.target).focus().click();
-  });
-
-  $('.menu.top button').on('click', e => {
-    $('.menu:not(.top)').hide();
-    $(e.target).next().show();
-  });
-
-  $('.nav').on('blur', _ => {
-    $('.menu:not(.top)').hide();
-  });
 
   $('[cmd]').on('click', e => {
     e.preventDefault();
+    e.stopPropagation();
     if ($(e.target).attr('cmd') !== '') {
-      $(document).blur();
-      location.replace(`?cmd=${$(e.target).attr('cmd')}`, false);
+      // $(document).blur();
+      // location.replace
+      console.log(`?cmd=${$(e.target).attr('cmd')}`);
     }
   });
 });
